bc09544bde771dce1b418d31d1a8f8a54cbd137f
eparisot <eparisot@student.42.fr>
2018-03-16 15:02:26 +0100

    AutoCommit

--- srcs_checker/checker.c
+++ srcs_checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 19:10:25 by eparisot          #+#    #+#             */
-/*   Updated: 2018/03/14 21:38:49 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/03/16 15:02:02 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	checker(t_list **lst_a)
 	lst_b = ft_lstnew(NULL, sizeof(int));
 	while (get_next_line(1, line))
 	{
-		if (!read_instruct(lst_a, &lst_b, *line))
+		if (!read_instru(lst_a, &lst_b, *line))
 		{
 			ft_printf("Error\n");
 			break ;
@@ -50,7 +50,7 @@ void	checker(t_list **lst_a)
 	free(line);
 }
 
-int		read_instruct(t_list **lst_a, t_list **lst_b, char *instruct)
+int		read_instru(t_list **lst_a, t_list **lst_b, char *instruct)
 {
 	if (ft_strstr("sa-sb-ss-pa-pb-ra-rb-rr-rra-rrb-rrr", instruct))
 	{
@@ -65,7 +65,7 @@ int		read_instruct(t_list **lst_a, t_list **lst_b, char *instruct)
 		(!ft_strcmp(instruct, "rra")) ? rra(lst_a) : 0;
 		(!ft_strcmp(instruct, "rrb")) ? rrb(lst_b) : 0;
 		(!ft_strcmp(instruct, "rrr")) ? rrr(lst_a, lst_b) : 0;
-		//////////
+///////////
 		(!ft_strcmp(instruct, "")) ? instruct = "End" : 0;
 		ft_printf("instruction : %s\n", instruct);
 		ft_printf("--\n");
