47d71ffcada8e0d16cd6f27beb8ab0eec71f4c30
eparisot <eparisot@student.42.fr>
2018-03-29 01:29:36 +0200

    auto-commit

--- srcs_pushswap/pushswap.c
+++ srcs_pushswap/pushswap.c
@@ -6,49 +6,20 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 19:10:25 by eparisot          #+#    #+#             */
-/*   Updated: 2018/03/28 23:57:19 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/03/29 01:25:05 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 #include "../srcs_common/common.h"
-/*
-static int	io(t_list *lst_a)
-{
-	while (lst_a->next)
-	{
-		if ((*(int*)lst_a->next->content) > (*(int*)lst_a->content))
-			return (0);
-		lst_a = lst_a->next;
-	}
-	return (1);
-}
 
-static int	iro(t_list *lst_a)
-{
-	while (lst_a->next)
-	{
-		if ((*(int*)lst_a->next->content) < (*(int*)lst_a->content))
-			return (0);
-		lst_a = lst_a->next;
-	}
-	return (1);
-}
-
-static int	blastval(t_list *lst)
-{
-	while (lst->next->next)
-		lst = lst->next;
-	return (*((int*)lst->content));
-}
-*/
 static int	lastval(t_list *lst)
 {
 	while (lst->next)
 		lst = lst->next;
 	return (*((int*)lst->content));
 }
-/*
+
 static int	isinhalf(t_list *lst, int nb)
 {
 	int		i;
@@ -76,126 +47,18 @@ static int	isinhalf(t_list *lst, int nb)
 	return (k - c);
 }
 
-static void	select_sort(t_list **lst_a, t_list **lst_b)
+static int	hmb(t_list *lst, int nb)
 {
-	int		min;
-	int		max;
-	int		i;
+	int	count;
 
-	min = ft_lstmin(*lst_a);
-	max = ft_lstmax(*lst_a);
-	i = 0;
-	while ((*lst_a)->content != NULL)
-	{
-		if (lastval(*lst_a) == min || lastval(*lst_a) == max)
-		{
-			if (lastval(*lst_a) == max)
-			{
-				pb(*lst_a, *lst_b);
-				ft_printf("pb\n");
-				rb(lst_b);
-				ft_printf("rb\n");
-				i++;
-			}
-			else if (lastval(*lst_a) == min)
-			{
-				pb(*lst_a, *lst_b);
-				ft_printf("pb\n");
-			}
-			min = ft_lstmin(*lst_a);
-			max = ft_lstmax(*lst_a);
-		}
-		else if (*((int*)(*lst_a)->content) == min || \
-		*((int*)(*lst_a)->content) == max)
-		{
-			if (*((int*)(*lst_a)->content) == max)
-			{
-				rra(lst_a);
-				ft_printf("rra\n");
-				pb(*lst_a, *lst_b);
-				ft_printf("pb\n");
-				rb(lst_b);
-				ft_printf("rb\n");
-				i++;
-			}
-			else if (*((int*)(*lst_a)->content) == min)
-			{
-				rra(lst_a);
-				ft_printf("rra\n");
-				pb(*lst_a, *lst_b);
-				ft_printf("pb\n");
-			}
-			min = ft_lstmin(*lst_a);
-			max = ft_lstmax(*lst_a);
-		}
-		else if (isinhalf(*lst_a, min) > 0 && isinhalf(*lst_a, max) > 0)
-		{
-			rra(lst_a);
-			ft_printf("rra\n");
-		}
-		else if (isinhalf(*lst_a, min) < 0 && isinhalf(*lst_a, max) < 0)
-		{
-			ra(lst_a);
-			ft_printf("ra\n");
-		}
-		else if (isinhalf(*lst_a, min) > 0 && isinhalf(*lst_a, max) < 0)
-		{
-			if (isinhalf(*lst_a, min) < -1 * isinhalf(*lst_a, max))
-			{
-				rra(lst_a);
-				ft_printf("rra\n");
-			}
-			else
-			{
-				ra(lst_a);
-				ft_printf("ra\n");
-			}
-		}
-		else if (isinhalf(*lst_a, min) < 0 && isinhalf(*lst_a, max) > 0)
-		{
-			if (isinhalf(*lst_a, min) * -1 < isinhalf(*lst_a, max))
-			{
-				ra(lst_a);
-				ft_printf("ra\n");
-			}
-			else
-			{
-				rra(lst_a);
-				ft_printf("rra\n");
-			}
-		}
-	}
-	if (isinhalf(*lst_b, ft_lstmax(*lst_b)))
-		while (i)
-		{
-			rrb(lst_b);
-			ft_printf("rrb\n");
-			i--;
-		}
-	else
-		while (ft_lstcount(*lst_b) - i)
-		{
-			rb(lst_b);
-			ft_printf("rb\n");
-			i++;
-		}
-	while ((*lst_b)->content)
-	{
-		pa(*lst_a, *lst_b);
-		ft_printf("pa\n");
-	}
-}
-*/
-
-static int	gotneg(t_list *lst)
-{
+	count = 0;
 	while (lst)
 	{
-		if (*((int*)lst->content) < 0)
-			return (1);
+		if (*((int*)lst->content) <= nb)
+			count++;
 		lst = lst->next;
 	}
-	return (0);
+	return (count);
 }
 
 static void	sort(t_list **lst_a, t_list **lst_b)
@@ -204,26 +67,93 @@ static void	sort(t_list **lst_a, t_list **lst_b)
 	int		max;
 	int		med;
 	int		c;
-	int		neg;
 
 	med = 0;
-	neg = gotneg(*lst_a);
 	while ((int*)(*lst_a)->content)
 	{
 		min = ft_lstmin(*lst_a);
 		max = ft_lstmax(*lst_a);
-		med = (neg * min) + ((max - min) / 2);
-		c = ft_lstcount(*lst_a);
+		med = min + ((max - min) / 2);
+		c = hmb(*lst_a, med);
 		while (c)
 		{
 			if (lastval(*lst_a) <= med)
 			{
 				pb(*lst_a, *lst_b);
 				ft_printf("pb\n");
+				c--;
 			}
 			rra(lst_a);
 			ft_printf("rra\n");
+		}
+	}
+	min = ft_lstmin(*lst_b);
+	max = ft_lstmax(*lst_b);
+	med = min + ((max - min) / 2);
+	c = ft_lstcount(*lst_b) - hmb(*lst_b, med) + 1;
+	while (c)
+	{
+		if (lastval(*lst_b) == max)
+		{
+			pa(*lst_a, *lst_b);
+			ft_printf("pa\n");
+			max = ft_lstmax(*lst_b);
 			c--;
+		}
+		else if (isinhalf(*lst_b, max) >= 0)
+		{
+			rrb(lst_b);
+			ft_printf("rrb\n");
+		}
+		else
+		{
+			rb(lst_b);
+			ft_printf("rb\n");
+		}
+	}
+	min = ft_lstmin(*lst_b);
+	med = min + ((ft_lstmax(*lst_b) - min) / 2);
+	c = ft_lstcount(*lst_b) - hmb(*lst_b, med);
+	max = c;
+	while (c)
+	{
+		if (lastval(*lst_b) >= med)
+		{
+			pa(*lst_a, *lst_b);
+			ft_printf("pa\n");
+			c--;
+		}
+		rrb(lst_b);
+		ft_printf("rrb\n");
+	}
+	while (max)
+	{
+		pb(*lst_a, *lst_b);
+		ft_printf("pb\n");
+		max--;
+	}
+	min = ft_lstmin(*lst_b);
+	max = ft_lstmax(*lst_b);
+	med = min + ((max - min) / 2);
+	c = ft_lstcount(*lst_b);
+	while (c)
+	{
+		if (lastval(*lst_b) == max)
+		{
+			pa(*lst_a, *lst_b);
+			ft_printf("pa\n");
+			max = ft_lstmax(*lst_b);
+			c--;
+		}
+		else if (isinhalf(*lst_b, max) >= 0)
+		{
+			rrb(lst_b);
+			ft_printf("rrb\n");
+		}
+		else
+		{
+			rb(lst_b);
+			ft_printf("rb\n");
 		}
 	}
 }
