e6e1066e6d2016bc68e4fcc757e4520699a2a441
eparisot <eparisot@student.42.fr>
2018-03-29 15:50:47 +0200

    auto-commit

--- srcs_pushswap/pushswap.c
+++ srcs_pushswap/pushswap.c
@@ -6,20 +6,38 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 19:10:25 by eparisot          #+#    #+#             */
-/*   Updated: 2018/03/29 01:25:05 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/03/29 15:48:09 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 #include "../srcs_common/common.h"
-
+/*
+static int	io(t_list *lst)
+{
+	while (lst)
+	{
+		if (*((int*)lst->next->content) > *((int*)lst->content))
+			return (0);
+		lst = lst->next;
+	}
+	return (1);
+}
+*/
 static int	lastval(t_list *lst)
 {
 	while (lst->next)
 		lst = lst->next;
 	return (*((int*)lst->content));
 }
-
+/*
+static int	blastval(t_list *lst)
+{
+	while (lst->next->next)
+		lst = lst->next;
+	return (*((int*)lst->content));
+}
+*/
 static int	isinhalf(t_list *lst, int nb)
 {
 	int		i;
