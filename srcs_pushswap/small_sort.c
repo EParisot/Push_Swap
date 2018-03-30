b3a8001e6a51e759b4dca815ba1ce3657c82a4e7
eparisot <eparisot@student.42.fr>
2018-03-30 19:51:34 +0200

    auto-commit

--- srcs_pushswap/small_sort.c
+++ srcs_pushswap/small_sort.c
@@ -6,26 +6,37 @@
 /*   By: eparisot <eparisot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 19:10:25 by eparisot          #+#    #+#             */
-/*   Updated: 2018/03/30 19:33:01 by eparisot         ###   ########.fr       */
+/*   Updated: 2018/03/30 19:50:45 by eparisot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 #include "../srcs_common/common.h"
 
+static void	ss1(t_list **lst_a, t_list **lst_b, int c)
+{
+	if (lastval(*lst_a) > blastval(*lst_a))
+	{
+		sa(lst_a);
+		ft_printf("sa\n");
+	}
+	c = ft_lstcount(*lst_b);
+	while (c)
+	{
+		pa(*lst_a, *lst_b);
+		ft_printf("pa\n");
+		c--;
+	}
+}
+
 void		small_sort(t_list **lst_a, t_list **lst_b)
 {
 	int		c;
 	int		min;
-	int		max;
-	int		med;
 
 	min = ft_lstmin(*lst_a);
-	max = ft_lstmax(*lst_a);
-	med = min + ((max - min) / 2);
-	c = hmb(*lst_a, med);
+	c = hmb(*lst_a, min + ((ft_lstmax(*lst_a) - min) / 2));
 	while (c && !io(*lst_a))
-	{
 		if (lastval(*lst_a) == min)
 		{
 			pb(*lst_a, *lst_b);
@@ -43,17 +54,5 @@ void		small_sort(t_list **lst_a, t_list **lst_b)
 			ra(lst_a);
 			ft_printf("ra\n");
 		}
-	}
-	if (lastval(*lst_a) > blastval(*lst_a))
-	{
-		sa(lst_a);
-		ft_printf("sa\n");
-	}
-	c = ft_lstcount(*lst_b);
-	while (c)
-	{
-		pa(*lst_a, *lst_b);
-		ft_printf("pa\n");
-		c--;
-	}
+	ss1(lst_a, lst_b, c);
 }
